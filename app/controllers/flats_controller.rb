3efee37571ba33acf1ccee81a29998e18a29ddcd
Thomas Stoll <stolltho@gmail.com>
2026-01-29 12:53:59 +1000

    add picture_url

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -48,6 +48,6 @@ class FlatsController < ApplicationController
   end
 
   def flat_params
-    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture_url)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,14 @@ puts "Cleaning database..."
 Flat.destroy_all
 
 puts "Creating flats..."
+
 Flat.create!(
   name: "Sunny Loft",
   address: "12 King Street, Sydney",
   description: "Bright loft close to the city.",
   price_per_night: 180,
-  number_of_guests: 2
+  number_of_guests: 2,
+  picture_url: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?w=800"
 )
 
 Flat.create!(
@@ -25,7 +27,8 @@ Flat.create!(
   address: "45 Brunswick Road, Melbourne",
   description: "Small but comfortable studio.",
   price_per_night: 120,
-  number_of_guests: 2
+  number_of_guests: 2,
+  picture_url: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?w=800"
 )
 
 Flat.create!(
@@ -33,7 +36,8 @@ Flat.create!(
   address: "8 Ocean Drive, Bondi",
   description: "Apartment with sea views.",
   price_per_night: 250,
-  number_of_guests: 4
+  number_of_guests: 4,
+  picture_url: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=800"
 )
 
 Flat.create!(
@@ -41,7 +45,8 @@ Flat.create!(
   address: "22 Maple Avenue, Brisbane",
   description: "Spacious house for families.",
   price_per_night: 220,
-  number_of_guests: 6
+  number_of_guests: 6,
+  picture_url: "https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=800"
 )
 
 Flat.create!(
@@ -49,7 +54,8 @@ Flat.create!(
   address: "101 Collins Street, Melbourne",
   description: "Luxury penthouse in the CBD.",
   price_per_night: 400,
-  number_of_guests: 4
+  number_of_guests: 4,
+  picture_url: "https://images.unsplash.com/photo-1493809842364-78817add7ffb?w=800"
 )
 
 puts "Finished! Created #{Flat.count} flats."
