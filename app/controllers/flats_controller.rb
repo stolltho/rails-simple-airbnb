000f9b8f3306a9b4009b5b3304782aa2b736b1c1
Thomas Stoll <stolltho@gmail.com>
2026-01-29 12:21:12 +1000

    add basic controller-view crud setup

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,12 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: %i[show edit]
+  before_action :set_flat, only: %i[show edit update destroy]
 
   def index
     @flats = Flat.all
   end
 
   def show
+    # @flat = Flat.find(params[:id])
   end
 
   def new
@@ -13,15 +14,31 @@ class FlatsController < ApplicationController
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    # @flat = Flat.find(params[:id])
   end
 
   def update
+    # @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    # @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
   end
 
   private
@@ -31,6 +48,6 @@ class FlatsController < ApplicationController
   end
 
   def flat_params
-    params.require(:flat).permit(:name)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root to: "flats#index"
+  root "flats#index"
   resources :flats
 
 end
